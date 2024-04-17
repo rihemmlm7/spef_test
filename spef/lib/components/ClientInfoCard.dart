6d9a59b92a3b0963afd51515d6073a252b40145a
Your Name <you@example.com>
2024-04-17 14:43:55 +0100

    update

--- spef/lib/components/ClientInfoCard.dart
+++ spef/lib/components/ClientInfoCard.dart
@@ -18,7 +18,7 @@ class ClientListItem extends StatelessWidget {
                   shape: BoxShape.circle,
                 ),
                 child: Image.network(
-                  '',
+                  'https://th.bing.com/th/id/R.fa0ca630a6a3de8e33e03a009e406acd?rik=UOMXfynJ2FEiVw&riu=http%3a%2f%2fwww.clker.com%2fcliparts%2ff%2fa%2f0%2fc%2f1434020125875430376profile.png&ehk=73x7A%2fh2HgYZLT1q7b6vWMXl86IjYeDhub59EZ8hF14%3d&risl=&pid=ImgRaw&r=0',
                   fit: BoxFit.cover,
                 ),
               ),
